a0b03165b7481b3cafa75d8519cd46456541a75e
Sunmock Yang <sunmock@live.ca>
2013-10-15 22:50:49 -0400

    Added basic behaviours to the camera so that it will always keep arm and mouth in view.

--- Assets/Scripts/CC_Behaviour.cs
+++ Assets/Scripts/CC_Behaviour.cs
@@ -4,17 +4,33 @@ using System.Collections;
 public class CC_Behaviour : MonoBehaviour {
 	
 	public enum SushiType {Sushi1, Sushi2, Sushi3, Sushi4, Sushi5, Sushi6, Sushi7, Sushi8};
-	
-	public GameObject[] sushi;
-	public GameObject[] chopsticks;
+
+	public CC_Sushi[] sushi {
+		get {
+			return level.sushiArray;
+		}
+	}
+
+	public CC_Chopstick[] chopsticks {
+		get {
+			return level.chopstickArray;
+		}
+	}
 	public GameObject[] players;
-	
+
+	public CC_Mouth mouth {
+		get {
+			return level.mouth;
+		}
+	}
+
 	public CC_Level level {
 		 get { 
 			if(CC_Level.instance != null) {
 				return CC_Level.instance; 
 			}  else {
-				Debug.LogError("FATAL: You need to have a CC_Level object in the scene to use level.");
+				//Debug.LogError("FATAL: You need to have a CC_Level object in the scene to use level.");
+				Debug.Log("FATAL: You need to have a CC_Level object in the scene to use level.");
 				return new CC_Level();
 			}
 		}
