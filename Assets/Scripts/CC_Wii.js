a63e90cfe0e39f832b508abf52ac1edda29e454b
Quang Nguyen <quwanger@dhcp-110-149.calabs-csit.carleton.ca>
2013-10-24 11:46:37 -0400

    The arm now rotates correctly with the Wiimote. Fixed the orientation of the arm as well. Nathan might be mad at me...

--- Assets/Scripts/CC_Wii.js
+++ Assets/Scripts/CC_Wii.js
@@ -1,4 +1,3 @@
-var wiimote				: Transform;
 static var whichRemote	: int;
 var motionPlus			: Transform;
 var WiiObject			: GameObject;
@@ -21,8 +20,10 @@ function Update () {
 	wiiAccel = Wii.GetWiimoteAcceleration(whichRemote);
 	
 	if(Wii.HasMotionPlus(whichRemote))
-	{
+	{	
 		if(Wii.IsMotionPlusCalibrated(whichRemote)) {
+			
+			Debug.Log("JDLS");
 			
 			motion = Wii.GetMotionPlus(whichRemote);
 			
@@ -31,22 +32,11 @@ function Update () {
 				motionPlus.localRotation = Quaternion.identity;
 			}
 			
-			motionPlus.RotateAround(motionPlus.position,motionPlus.right,motion.x);
+			motionPlus.RotateAround(motionPlus.position,motionPlus.right,-motion.x);
 			motionPlus.RotateAround(motionPlus.position,motionPlus.up,-motion.y);
-			motionPlus.RotateAround(motionPlus.position,motionPlus.forward,motion.z);
+			motionPlus.RotateAround(motionPlus.position,motionPlus.forward,-motion.z);
 			
 			Debug.Log(motion);
 		}
-		/*motion = Wii.GetMotionPlus(whichRemote);
-		
-		
-		
-		if(Input.GetKeyDown("space") || Wii.GetButtonDown(whichRemote,"HOME"))
-		{
-			motionPlus.localRotation = Quaternion.identity;
-		}
-		motionPlus.RotateAround(motionPlus.position,motionPlus.right,motion.x);
-		motionPlus.RotateAround(motionPlus.position,motionPlus.up,-motion.y);
-		motionPlus.RotateAround(motionPlus.position,motionPlus.forward,motion.z);*/
 	}
 }
