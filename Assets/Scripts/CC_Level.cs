31804f4cd2e163555d29419016e007d44d78e659
Paden Shorey <paden@studio236.ca>
2013-11-15 16:49:21 -0500

    Timer added

--- Assets/Scripts/CC_Level.cs
+++ Assets/Scripts/CC_Level.cs
@@ -13,8 +13,12 @@ public class CC_Level : MonoBehaviour {
 	public bool DebugMode = true;
 	
 	public int playerScore = 0;
-	
 	public int pointsToAdd = 0;
+	
+	//points needed to beat the level
+	public int pointsToWin = 1000000;
+	//time the player has to beat the level
+	public int levelTime = 180;
 	
 	public static CC_Level instance { get; private set;}
 	
--- Assets/Scripts/CC_Timer.cs
+++ Assets/Scripts/CC_Timer.cs
@@ -2,13 +2,23 @@ using UnityEngine;
 using System.Collections;
 
 public class CC_Timer : MonoBehaviour {
-
+	
+	private float t;
+	
 	// Use this for initialization
 	void Start () {
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {//this.GetComponent<GUIText>().text = GameObject.Find("CC_Level").GetComponent<CC_TimeManager>().GetCurrentTimeString();
+	void Update () {
+		t = GameObject.Find("CC_Level").GetComponent<CC_TimeManager>().timeInMillis;
+		this.GetComponent<GUIText>().text = GameObject.Find("CC_Level").GetComponent<CC_TimeManager>().GetCurrentTimeString();
+		
+		Color c;
+		
+		if(t < 60){
+			this.GetComponent<GUIText>().material.color = Color.red;
+		}
 	}
 }
