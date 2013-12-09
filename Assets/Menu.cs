f1247b234c71ff25a4abd464f1b2eb9fbf1ef944
Nathan <nathan@studio236.ca>
2013-12-09 18:38:22 -0500

    FinishedMenuSystem and Added mapping for 2nd controller

--- Assets/Menu.cs
+++ Assets/Menu.cs
@@ -8,24 +8,21 @@ public class Menu : MonoBehaviour {
 	public Texture Overlay;
 	public int index = 0;
 	public int optionsIndex = 0;
+	public int inputIndex = 0;
+	
 	public GameObject start;
 	public GameObject level;
 	public GameObject option;
 	
-	public GameObject wii;
-	public GameObject xbox;
-	
+	public GameObject inputWiiText;
+	public GameObject inputXboxText;
+	public GameObject inputKeyboardText;
+	private string levelSelected = "main";
 	private Color gold;
-	//public GameObject chopstick;
-	//private Vector3 startPos;
-	//private Vector3 optionsPos;
-	//private Vector3 levelPos;
 	private bool mmActive = true;
 	private bool optionsActive = false;
-	
-	//public Transform target;
-	//public Transform target2;
-
+	private bool levelsActive = false;
+	private bool inputActive = false;
 	
 	// 0 = top 
 	// 1 = middle
@@ -34,15 +31,11 @@ public class Menu : MonoBehaviour {
 	void Awake() {
 		AudioListener.volume = 0.0f;
 		gold = new Color(255,246,0);
-		//startPos = new Vector3(chopstick.transform.position.x, GameObject.Find("pos1").transform.position.y, chopstick.transform.position.z);
-		//levelPos = new Vector3(chopstick.transform.position.x, GameObject.Find("pos2").transform.position.y, chopstick.transform.position.z);
-		//optionsPos = new Vector3(chopstick.transform.position.x, GameObject.Find("pos3").transform.position.y, chopstick.transform.position.z);
-		
-		mainMenu();
 	}
 	
 	void Start(){
 		this.gameObject.GetComponent<CC_SoundManager>().playSound(CC_Level.SoundType.intro);
+		mainMenu();
 	}
 	
 	IEnumerator doFade()
@@ -61,57 +54,47 @@ public class Menu : MonoBehaviour {
 		GUI.DrawTexture( new Rect(0, 0, Screen.width, Screen.height ), Overlay);
 	}
 	
-	void mainMenu()
-	{	
-		optionsActive = false;
-		GameObject.Find("level1").guiTexture.enabled = false;
-		GameObject.Find("level2").guiTexture.enabled = false;
-		GameObject.Find("level3").guiTexture.enabled = false;	
-		GameObject.Find("level4").guiTexture.enabled = false;
-		
-		GameObject.Find("options").guiText.enabled = true;
-		GameObject.Find("start").guiText.enabled = true;
-		GameObject.Find("level").guiText.enabled = true;
-		
-		GameObject.Find("wii").guiText.enabled = false;
-		GameObject.Find("xbox").guiText.enabled = false;
-		
-		GameObject.Find("wiiImage").guiTexture.enabled = false;
-		GameObject.Find("xboxImage").guiTexture.enabled = false;
-//		GameObject.Find("Chopstick").renderer.enabled = true;
-		
-		index = 0;
-		//iTween.MoveTo(chopstick, iTween.Hash("position",startPos, "x", 0.0f, "onComplete", "TweenComplete", "onCompleteTarget", gameObject));
-		
-		mmActive = true;
-	}
 	
 	void Update () {
 		StartCoroutine(doFade());
-		Debug.Log(Screen.width);
-		
-		if(index==0)
-		{
-			start.guiText.material.SetColor("_Color", gold);
-			level.guiText.material.SetColor("_Color", Color.white);
-			option.guiText.material.SetColor("_Color", Color.white);
-
-		}
-		
-		if(index==1)
-		{
-			start.guiText.material.SetColor("_Color", Color.white);
-			level.guiText.material.SetColor("_Color", gold);
-			option.guiText.material.SetColor("_Color", Color.white);
-		}
-		
-		if(index==2)
-		{
-			start.guiText.material.SetColor("_Color", Color.white);
-			level.guiText.material.SetColor("_Color", Color.white);
-			option.guiText.material.SetColor("_Color", gold);
-		}
+		Debug.Log(Screen.width);	
 			
+		if(inputActive)
+		{	
+			if(Input.GetKeyDown(KeyCode.K))
+			{
+				this.gameObject.GetComponent<CC_SoundManager>().playSound(CC_Level.SoundType.hover);
+				if(inputIndex == 1){
+					inputIndex -= 1;
+				}
+				
+				else if(inputIndex == 2){
+					inputIndex -= 1;
+				}	
+			}
+	
+			if(Input.GetKeyDown(KeyCode.L))
+			{	
+				this.gameObject.GetComponent<CC_SoundManager>().playSound(CC_Level.SoundType.hover);
+				if(inputIndex == 1){
+					inputIndex += 1;	
+				}
+				
+				else if(inputIndex == 0){
+					inputIndex += 1;
+				}
+			}
+			
+			if(Input.GetKeyDown(KeyCode.Return))
+			{
+				this.gameObject.GetComponent<CC_SoundManager>().playSound(CC_Level.SoundType.accept);
+				Application.LoadLevel(levelSelected);				
+			}
+			
+			textHighlighting(inputIndex);
+			instructionImages(inputIndex);
+		}
+		
 		if(mmActive)
 		{
 			if(Input.GetKeyDown(KeyCode.W))
@@ -119,13 +102,10 @@ public class Menu : MonoBehaviour {
 				this.gameObject.GetComponent<CC_SoundManager>().playSound(CC_Level.SoundType.hover);
 				if(index == 1){
 					index -= 1;
-					
-					//iTween.MoveTo(chopstick, iTween.Hash("position",startPos, "time", 0.0f, "onComplete", "TweenComplete", "onCompleteTarget", gameObject));
 				}
 				
 				else if(index == 2){
 					index -= 1;
-					//iTween.MoveTo(chopstick, iTween.Hash("position",levelPos, "time", 0.0f, "onComplete", "TweenComplete", "onCompleteTarget", gameObject));	
 				}	
 			}
 	
@@ -134,27 +114,32 @@ public class Menu : MonoBehaviour {
 				this.gameObject.GetComponent<CC_SoundManager>().playSound(CC_Level.SoundType.hover);
 				if(index == 1){
 					index += 1;
-					//iTween.MoveTo(chopstick, iTween.Hash("position",optionsPos, "time", 0.0f, "onComplete", "TweenComplete", "onCompleteTarget", gameObject));	
 				}
 				
 				else if(index == 0){
 					index += 1;
-					//iTween.MoveTo(chopstick, iTween.Hash("position",levelPos, "time", 0.0f, "onComplete", "TweenComplete", "onCompleteTarget", gameObject));
 				}
 			}
 		
-		
 			if(index == 0)
 			{
+				start.guiText.material.SetColor("_Color", gold);
+				level.guiText.material.SetColor("_Color", Color.white);
+				option.guiText.material.SetColor("_Color", Color.white);
+				
 				if(Input.GetKeyDown(KeyCode.Return))
 				{
 					this.gameObject.GetComponent<CC_SoundManager>().playSound(CC_Level.SoundType.accept);
-					Application.LoadLevel("main");
+					inputSelection();
 				}
 			}
 			
 			if(index == 1)
 			{
+				start.guiText.material.SetColor("_Color", Color.white);
+				level.guiText.material.SetColor("_Color", gold);
+				option.guiText.material.SetColor("_Color", Color.white);
+
 				if(Input.GetKeyDown(KeyCode.Return))
 				{
 					this.gameObject.GetComponent<CC_SoundManager>().playSound(CC_Level.SoundType.accept);
@@ -164,6 +149,10 @@ public class Menu : MonoBehaviour {
 			
 			if(index == 2)
 			{
+				start.guiText.material.SetColor("_Color", Color.white);
+				level.guiText.material.SetColor("_Color", Color.white);
+				option.guiText.material.SetColor("_Color", gold);
+
 				if(Input.GetKeyDown(KeyCode.Return))
 				{
 					this.gameObject.GetComponent<CC_SoundManager>().playSound(CC_Level.SoundType.accept);
@@ -180,114 +169,210 @@ public class Menu : MonoBehaviour {
 				this.gameObject.GetComponent<CC_SoundManager>().playSound(CC_Level.SoundType.back);
 			}
 		}
-		
-		
-		if(mmActive == false && optionsActive == false )
+			
+		if(levelsActive == true)
 		{
 			if(Input.GetKeyDown(KeyCode.Alpha1))
 			{
 				this.gameObject.GetComponent<CC_SoundManager>().playSound(CC_Level.SoundType.accept);
-				Application.LoadLevel("main");
+				levelSelected = "main";
+				inputSelection();
 			}
 			
 			if(Input.GetKeyDown(KeyCode.Alpha2))
 			{
 				this.gameObject.GetComponent<CC_SoundManager>().playSound(CC_Level.SoundType.accept);
-				Application.LoadLevel("Sushi_Restaurant");
+				levelSelected = "Sushi_Restaurant";
+				inputSelection();
 			}
 			
 			if(Input.GetKeyDown(KeyCode.Alpha3))
 			{
 				this.gameObject.GetComponent<CC_SoundManager>().playSound(CC_Level.SoundType.accept);
-				Application.LoadLevel("Hospital");
+				levelSelected = "Hospital";
+				inputSelection();
 			}
-
 		}
 		
 		// options
-		if(mmActive == false && optionsActive == true)
+		if(optionsActive == true)
 		{
-			if(optionsIndex==0)
-			{
-				wii.guiText.material.SetColor("_Color", gold);
-				xbox.guiText.material.SetColor("_Color", Color.white);
-				GameObject.Find("wiiImage").guiTexture.enabled = true;
-				GameObject.Find("xboxImage").guiTexture.enabled = false;
-			}
-		
-			if(optionsIndex==1)
-			{
-				wii.guiText.material.SetColor("_Color", Color.white);
-				xbox.guiText.material.SetColor("_Color", gold);
-				GameObject.Find("wiiImage").guiTexture.enabled = false;
-				GameObject.Find("xboxImage").guiTexture.enabled = true;
-			}
-			
-			if(Input.GetKeyDown(KeyCode.W))
+			if(Input.GetKeyDown(KeyCode.K))
 			{
 				this.gameObject.GetComponent<CC_SoundManager>().playSound(CC_Level.SoundType.hover);
 				if(optionsIndex == 1){
 					optionsIndex -= 1;
-					
-					//iTween.MoveTo(chopstick, iTween.Hash("position",startPos, "time", 0.0f, "onComplete", "TweenComplete", "onCompleteTarget", gameObject));
+				}
+				
+				else if(optionsIndex == 2){
+					optionsIndex -= 1;
 				}	
 			}
 	
-			if(Input.GetKeyDown(KeyCode.S))
-			{
+			if(Input.GetKeyDown(KeyCode.L))
+			{	
 				this.gameObject.GetComponent<CC_SoundManager>().playSound(CC_Level.SoundType.hover);
-				if(optionsIndex == 0){
+				if(optionsIndex == 1){
+					optionsIndex += 1;	
+				}
+				
+				else if(optionsIndex == 0){
 					optionsIndex += 1;
-					//iTween.MoveTo(chopstick, iTween.Hash("position",levelPos, "time", 0.0f, "onComplete", "TweenComplete", "onCompleteTarget", gameObject));
 				}
 			}
 			
-			
-			
+			textHighlighting(optionsIndex);
+			instructionImages(optionsIndex);
 		}
+	}
+	
+	void textHighlighting(int tempIndex)
+	{
+		// input menu
+		if(tempIndex==0)
+		{
+			inputKeyboardText.guiText.material.SetColor("_Color", gold);
+			inputXboxText.guiText.material.SetColor("_Color", Color.white);
+			inputWiiText.guiText.material.SetColor("_Color", Color.white);
+		}
+		
+		if(tempIndex==1)
+		{
+			inputKeyboardText.guiText.material.SetColor("_Color", Color.white);
+			inputXboxText.guiText.material.SetColor("_Color", gold);
+			inputWiiText.guiText.material.SetColor("_Color", Color.white);
+		}
+			
+		if(tempIndex==2)
+		{
+			inputKeyboardText.guiText.material.SetColor("_Color", Color.white);
+			inputXboxText.guiText.material.SetColor("_Color", Color.white);
+			inputWiiText.guiText.material.SetColor("_Color", gold);
+		}
+	}
+	
+	void instructionImages(int tempIndex)
+	{
+		if(tempIndex==0)
+		{
+			GameObject.Find("wiiImage").guiTexture.enabled = true;
+			GameObject.Find("xboxImage").guiTexture.enabled = false;
+			GameObject.Find("keyboardImage").guiTexture.enabled = false;
+		}
+		
+		if(tempIndex==1)
+		{
+			GameObject.Find("wiiImage").guiTexture.enabled = false;
+			GameObject.Find("xboxImage").guiTexture.enabled = true;
+			GameObject.Find("keyboardImage").guiTexture.enabled = false;
+		}
+		
+		if(tempIndex==2)
+		{
+			GameObject.Find("wiiImage").guiTexture.enabled = false;
+			GameObject.Find("xboxImage").guiTexture.enabled = false;
+			GameObject.Find("keyboardImage").guiTexture.enabled = true;
+		}
+	}
+	
+	void mainMenu()
+	{	
+		optionsActive = false;
+		mmActive = true;
+		inputActive = false;
+		levelsActive = false;
+		
+		GameObject.Find("level1").guiTexture.enabled = false;
+		GameObject.Find("level2").guiTexture.enabled = false;
+		GameObject.Find("level3").guiTexture.enabled = false;	
+		GameObject.Find("level4").guiTexture.enabled = false;
+		
+		GameObject.Find("options").guiText.enabled = true;
+		GameObject.Find("start").guiText.enabled = true;
+		GameObject.Find("level").guiText.enabled = true;
+
+		GameObject.Find("wiiImage").guiTexture.enabled = false;
+		GameObject.Find("xboxImage").guiTexture.enabled = false;
+		GameObject.Find("keyboardImage").guiTexture.enabled = false;
+		
+		GameObject.Find("inputKeyboard").guiText.enabled = false;
+		GameObject.Find("inputWii").guiText.enabled = false;
+		GameObject.Find("inputXbox").guiText.enabled = false;
+		
+		GameObject.Find("logo").guiTexture.enabled = true;
+		
+		index = 0;		
+		mmActive = true;
 	}
 	
 	void levelSelect()
 	{
+		optionsActive = false;
 		mmActive = false;
+		inputActive = false;
+		levelsActive = true;
+		
 		GameObject.Find("options").guiText.enabled = false;
 		GameObject.Find("start").guiText.enabled = false;
 		GameObject.Find("level").guiText.enabled = false;
-		//GameObject.Find("Chopstick").renderer.enabled = false;
 		
 		GameObject.Find("level1").guiTexture.enabled = true;
 		GameObject.Find("level2").guiTexture.enabled = true;
 		GameObject.Find("level3").guiTexture.enabled = true;
 		GameObject.Find("level4").guiTexture.enabled = true;
 		
-		GameObject.Find("wii").guiText.enabled = false;
-		GameObject.Find("xbox").guiText.enabled = false;
-		
 		GameObject.Find("wiiImage").guiTexture.enabled = false;
 		GameObject.Find("xboxImage").guiTexture.enabled = false;
+		GameObject.Find("keyboardImage").guiTexture.enabled = false;
+
+		GameObject.Find("logo").guiTexture.enabled = false;
 	}
 	
 	void options()
 	{
-		optionsIndex = 0;
-		mmActive = false;
 		optionsActive = true;
+		mmActive = false;
+		inputActive = false;
+		levelsActive = false;
+		
+		optionsIndex = 0;
 		GameObject.Find("options").guiText.enabled = false;
 		GameObject.Find("start").guiText.enabled = false;
 		GameObject.Find("level").guiText.enabled = false;
-		//GameObject.Find("Chopstick").renderer.enabled = false;
 		
 		GameObject.Find("level1").guiTexture.enabled = false;
 		GameObject.Find("level2").guiTexture.enabled = false;
 		GameObject.Find("level3").guiTexture.enabled = false;
 		GameObject.Find("level4").guiTexture.enabled = false;
 		
-		GameObject.Find("wii").guiText.enabled = true;
-		GameObject.Find("xbox").guiText.enabled = true;
-	}
-
-}
-
-
-
+		GameObject.Find("inputKeyboard").guiText.enabled = true;
+		GameObject.Find("inputWii").guiText.enabled = true;
+		GameObject.Find("inputXbox").guiText.enabled = true;
 		
+		GameObject.Find("logo").guiTexture.enabled = false;
+	}
+	
+	void inputSelection()
+	{
+		optionsActive = false;
+		mmActive = false;
+		inputActive = true;
+		levelsActive = false;
+		
+		inputIndex = 0;
+		GameObject.Find("options").guiText.enabled = false;
+		GameObject.Find("start").guiText.enabled = false;
+		GameObject.Find("level").guiText.enabled = false;
+		
+		GameObject.Find("level1").guiTexture.enabled = false;
+		GameObject.Find("level2").guiTexture.enabled = false;
+		GameObject.Find("level3").guiTexture.enabled = false;
+		GameObject.Find("level4").guiTexture.enabled = false;
+
+		GameObject.Find("inputKeyboard").guiText.enabled = true;
+		GameObject.Find("inputWii").guiText.enabled = true;
+		GameObject.Find("inputXbox").guiText.enabled = true;
+		
+		GameObject.Find("logo").guiTexture.enabled = false;
+	}
+}
